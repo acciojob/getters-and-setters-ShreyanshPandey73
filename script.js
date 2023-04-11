ce67a4307757c3fa7099ebb85459717c336a6ccc
ShreyanshPandey73 <117717418+ShreyanshPandey73@users.noreply.github.com>
2023-04-11 10:27:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this.name = name;
-		this.age = age;
+		this._name = name;
+		this._age = age;
 	}
 	get name(){
-		return this.name;
+		return this._name;
 	}
 	set age(age){
-		this.age = age;
+		this._age = age;
 	}
 }
 
 class Student extends Person {
   study() {
-    console.log(${this.name} 'is studying');
+    console.log(${this.name} +' is studying');
   }
 }
 
 class Teacher extends Person {
   teach() {
-    console.log(${this.name} ' is teaching');
+    console.log(${this.name} +' is teaching');
   }
 }
 
@@ -28,3 +28,6 @@ class Teacher extends Person {
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
+
+
